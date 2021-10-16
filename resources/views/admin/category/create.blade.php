1003f0e508aa55505b73532fb49e85cdf3191142
Nguyễn Văn Duy <duysangtao2001@gmail.com>
2021-10-16 16:09:35 +0700

    up

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -9,10 +9,44 @@
     <div class="content mt-2">
         <div class="animated fadeIn">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-6">
                     <div class="card">
                         <div class="card-header">
-                        <h3><strong>Thêm loại sản phẩm</strong> </h3>
+                        <h3><strong>Danh mục cha</strong> </h3>
+                        </div>
+                        <div class="card-body card-block">
+                            <form action="{{route('category.store')}}" method="post" enctype="multipart/form-data" class="form-horizontal">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                            <!-- @method('PATCH') -->
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Tên loại sản phẩm</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="name" placeholder="........" class="form-control" required></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="file-input" class=" form-control-label">Ảnh đại diện</label></div>
+                                    <div class="col-12 col-md-9"><input type="file" id="file-input" name="img" class="form-control-file" required></div>
+                                </div>
+                                <div class="row form-group">
+                                <div class="col col-md-3"><label for="select" class=" form-control-label">An hien</label></div>
+                                <div class="col-12 col-md-9">
+                                    <select name="status" id="select" class="form-control">
+                                        <option value="1">Hiện</option> 
+                                        <option value="0">Ẩn </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <br>
+                            <input class="btn btn-success float-right" type="submit" value="Thêm">
+                            </form>      
+                        </div>          
+                    </div><!-- .content -->
+                </div><!-- .content -->
+
+                
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-header">
+                        <h3><strong>Danh mục con</strong> </h3>
                         </div>
                         <div class="card-body card-block">
                             <form action="{{route('category.store')}}" method="post" enctype="multipart/form-data" class="form-horizontal">
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -12,80 +12,102 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header">
-                        <h3><strong>Thêm sản phẩm mới</strong> </h3>
+                            <h3><strong>Thêm sản phẩm mới</strong> </h3>
                         </div>
                         <div class="card-body card-block">
                             <form action="{{route('product.store')}}" method="post" enctype="multipart/form-data" class="form-horizontal">
-                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <input type="hidden" name="_token" value="{{csrf_token()}}">
                             <!-- @method('PATCH') -->
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Tên loại sản phẩm</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="name_pro" placeholder="........" class="form-control"></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="file-input" class=" form-control-label">Ảnh  đại diện</label></div>
-                                    <div class="col-12 col-md-9"><img title="Chọn ảnh đại diện" style="cursor: pointer;" class="img_preview" src="{{asset('image/no-img.png')}}" alt=""></div>
-                                    <div class="col-12 col-md-9"><input hidden class="img" type="file" id="file-input" name="img" onchange="changeImg(this)" class="form-control-file"></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="file-input" class=" form-control-label">Ảnh liên quan</label></div>
-                                    <div class="col-12 col-md-9"><input type="file" id="file-input" name="img_gallery[]" class="form-control-file" accept="image/*" multiple></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Giá gốc</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="price" placeholder="........" class="form-control"></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Giá khuyến mãi</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="price_sale" placeholder="........" class="form-control"></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Số lượng</label></div>
-                                    <div class="col-12 col-md-9"><input type="text" id="text-input" name="quantity" placeholder="........" class="form-control"></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="select" class="form-control-label">An hien</label></div>
-                                    <div class="col-12 col-md-9">
-                                        <select name="status" id="select" class="form-control">
-                                            <option value="1">Hiện</option> 
-                                            <option value="0">Ẩn </option>
-                                        </select>
+
+                               <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Tên loại sản phẩm</label></div>
+                                            <div class="col-12 col-md-12"><input type="text" id="text-input" name="name_pro" placeholder="........" class="form-control"></div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Giá gốc</label></div>
+                                            <div class="col-12 col-md-12"><input type="text" id="text-input" name="price" placeholder="........" class="form-control"></div>
+                                        </div>
+        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Giá khuyến mãi</label></div>
+                                            <div class="col-12 col-md-12"><input type="text" id="text-input" name="price_sale" placeholder="........" class="form-control"></div>
+                                        </div>
+
+                                        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Số lượng</label></div>
+                                            <div class="col-12 col-md-12"><input type="text" id="text-input" name="quantity" placeholder="........" class="form-control"></div>
+                                        </div>
+
+                                        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Ngày nhập sản phẩm</label></div>
+                                            <div class="col-12 col-md-12"><input type="date" id="text-input" name="add_day" placeholder="........" class="form-control" ></div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="name" class=" form-control-label">Ngày hết hạn</label></div>
+                                            <div class="col-12 col-md-12"><input type="date" id="text-input" name="expired_day" placeholder="........" class="form-control" ></div>
+                                        </div>
+
                                     </div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="select" class=" form-control-label">Danh mục</label></div>
-                                    <div class="col-12 col-md-9">
-                                        <select name="category" id="select" class="form-control">
-                                            @foreach ($cate as $val)
-                                                <option value="{{$val->id}}">{{$val->name}}</option> 
-                                            @endforeach 
-                                        </select>
+
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="file-input" class=" form-control-label">Ảnh  đại diện</label></div>
+                                            <div class="col-12 col-md-12"><img title="Chọn ảnh đại diện" style="cursor: pointer;" class="img_preview" src="{{asset('image/no-img.png')}}" alt=""></div>
+                                            <div class="col-12 col-md-12"><input hidden class="img" type="file" id="file-input" name="img" onchange="changeImg(this)" class="form-control-file"></div>
+                                        </div>
+        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="file-input" class=" form-control-label">Ảnh liên quan</label></div>
+                                            <div class="col-12 col-md-12"><input type="file" id="file-input" name="img_gallery[]" class="form-control-file" accept="image/*" multiple></div>
+                                        </div>
+        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="select" class="form-control-label">An hien</label></div>
+                                            <div class="col-12 col-md-12">
+                                                <select name="status" id="select" class="form-control">
+                                                    <option value="1">Hiện</option> 
+                                                    <option value="0">Ẩn </option>
+                                                </select>
+                                            </div>
+                                        </div>
+        
+                                        <div class="form-group">
+                                            <div class="col col-md-6"><label for="select" class=" form-control-label">Danh mục</label></div>
+                                            <div class="col-12 col-md-12">
+                                                <select name="category" id="select" class="form-control">
+                                                    @foreach ($cate as $val)
+                                                        <option value="{{$val->id}}">{{$val->name}}</option> 
+                                                    @endforeach 
+                                                </select>
+                                            </div>
+                                        </div>
+
+
                                     </div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Ngày nhập sản phẩm</label></div>
-                                    <div class="col-12 col-md-9"><input type="date" id="text-input" name="add_day" placeholder="........" class="form-control" ></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Ngày hết hạn</label></div>
-                                    <div class="col-12 col-md-9"><input type="date" id="text-input" name="expired_day" placeholder="........" class="form-control" ></div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Mô tả</label></div>
-                                    <div class="col-12 col-md-9"><textarea name="description" id="noidung" class="ckeditor"></textarea></div>
+                               </div>
+
+                               <div class=" form-group">
+                                    <div class="col col-md-6"><label for="name" class=" form-control-label">Mô tả</label></div>
+                                    <div class="col-12 col-md-12"><textarea name="description" id="noidung" class="ckeditor"></textarea></div>
                                     <script type="text/javascript">
                                         var editor = CKEDITOR.replace('noidung',{
-                                                filebrowserImageBrowseUrl : 'localhost:8000/admin/editor/ckfinder/ckfinder.html?type=Images',
-                                                filebrowserFlashBrowseUrl : 'localhost:8000/admin/editor/ckfinder/ckfinder.html?type=Flash',
-                                                filebrowserImageUploadUrl : 'localhost:8000/admin/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
-                                                filebrowserFlashUploadUrl : 'localhost:8000/admin/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
-                                      });
+                                            filebrowserImageBrowseUrl : 'localhost:8000/admin/editor/ckfinder/ckfinder.html?type=Images',
+                                            filebrowserFlashBrowseUrl : 'localhost:8000/admin/editor/ckfinder/ckfinder.html?type=Flash',
+                                            filebrowserImageUploadUrl : 'localhost:8000/admin/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images',
+                                            filebrowserFlashUploadUrl : 'localhost:8000/admin/editor/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
+                                        });
                                     </script>
-                                </div>
-                        </div>
-                            <br>
-                            <input class="btn btn-success float-right" type="submit" value="Thêm">
-                            </form>      
+                                </div>  
+                                <br>
+                                <input class="btn btn-success float-right" type="submit" value="Thêm">
+                            </form>   
+                        </div>   
                         </div>          
                     </div><!-- .content -->
                 </div><!-- .content -->
